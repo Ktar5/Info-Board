8aecdf58b6bdc2d874b5c480be2cc7919973a4df
pixar02 <pixar.pf@gmail.com>
2018-04-05 01:15:07 +0200

    More for API

--- src/main/java/com/infogroup/infoboard/api/API.java
+++ src/main/java/com/infogroup/infoboard/api/API.java
@@ -1,12 +1,17 @@
 package com.infogroup.infoboard.api;
 
 import com.infogroup.infoboard.InfoBoardReborn;
+import org.bukkit.entity.Player;
 
 public class API {
 
     private InfoBoardReborn plugin;
 
     public API(InfoBoardReborn plugin){ this.plugin = plugin; }
+//TODO finish
+    public void createBoard(Player player){}
+
+    public void updateBoard(Player player){}
 
 
 

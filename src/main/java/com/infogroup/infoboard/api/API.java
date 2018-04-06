f97179826be50a2aed95e29584bdb4af12c64a1e
pixar02 <pixar.pf@gmail.com>
2018-04-06 23:55:05 +0200

    Filling the API

--- src/main/java/com/infogroup/infoboard/api/API.java
+++ src/main/java/com/infogroup/infoboard/api/API.java
@@ -3,16 +3,25 @@ package com.infogroup.infoboard.api;
 import com.infogroup.infoboard.InfoBoardReborn;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class API {
 
     private InfoBoardReborn plugin;
+    private HashMap<Player, Integer> Boards = new HashMap<>();
 
     public API(InfoBoardReborn plugin){ this.plugin = plugin; }
-//TODO finish
-    public void createBoard(Player player){}
+    //TODO finish
+
+
+    public void createBoard(Player player, List<String> lines, String title){}
 
     public void updateBoard(Player player){}
 
+    public void removeBoard(Player player){}
 
+    public HashMap<Player, Integer> getBoards() { return this.Boards; }
 
+    public Integer getBoard(Player player){ return this.Boards.get(player); }
 }
